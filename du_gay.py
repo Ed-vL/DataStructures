fcc4658ac4d44762f0f6d6a60b66246f90e0c09b
Tay Balau <taynaravitorino@gmail.com>
2021-06-15 18:16:39 -0300

    Tay foda demais adicionando du_gay e bonitona

---
+++ du_gay.py
@@ -0,0 +1,11 @@
+# Du gay?
+
+print('O Du é gay?')
+
+resposta = input()
+
+while resposta != 'sim':
+   print('Nah, resposta errada. Tente novamente.')
+   print('O Du é gay?')
+   resposta = input()
+  
